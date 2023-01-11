4cddc9d86ed829e4ce169eb95a3c7f9ee7aa6112
kho96 <k____ho__@naver.com>
2023-01-11 12:32:25 +0900

    module수정

--- mod/mod1.py
+++ mod/mod1.py
@@ -6,6 +6,8 @@ def add(a, b):
 def sub(a, b):
     return a - b
 
-if __name__ == "__main__": # main인 경우에만 실행
+# main인 경우(mod1.py에서만)에만 실행, 
+# 이걸 안하면 import한 경우 다 실행됨
+if __name__ == "__main__": 
     print("mod1:" + str(add(1,2))) 
     print("mod1:" + str(add(3,4))) 
